8226945724b60de6539a4d59bc34bbb8126248a3
Mick <mick@heyup.fit>
2026-07-12 01:57:45 -0700

    Initial Commit

---
+++ HeyUp/HeyUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeyUpApp.swift
+//  HeyUp
+//
+//  Created by MAC on 12/7/26.
+//
+
+import SwiftUI
+
+@main
+struct HeyUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
